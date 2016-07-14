997dcb26b375cafe47a907c5e1e79d1708de2fdd
stephenCoderCamps <stephen.walther@codercamps.com>
2016-07-13 17:21:10 -0700

    added service sample

--- ClientApp/about.component.ts
+++ ClientApp/about.component.ts
@@ -1,9 +1,15 @@
 ﻿import {Component} from '@angular/core';
-
+import {TaxService} from './tax.service';
 
 @Component({
     selector: 'about',
-    template: require('./about.component.html')
+    template: require('./about.component.html'),
+    providers: [TaxService]
 })
 export class AboutComponent {
+    public tax: number;
+
+    constructor(taxService: TaxService) {
+        this.tax = taxService.calculate(5.00);
+    }
 }
